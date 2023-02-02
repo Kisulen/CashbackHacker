c39346b66eb2a7a63457bc0742a9bda4efc9b101
Maria Ustyuzhanina <elkaran@gmail.com>
2023-02-02 17:42:40 +0300

    junit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,31 @@
+package ru.netology.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class CashbackHackServiceTest {
+
+    @Test
+    public void remain() {
+        int amount = 500;
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 500;
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void remain1000() {
+        int amount = 1000;
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+}
